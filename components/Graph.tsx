9f442e8c3b883304e9a3e20704dd140ae259a389
James Mealy <jamesmealy1@gmail.com>
2022-10-04 14:20:48 +0100

    Chart the selected genre over time

--- components/Graph.tsx
+++ components/Graph.tsx
@@ -1,14 +1,59 @@
 import type { NextPage } from 'next';
 import type { GenreGraphData } from 'types';
+import moment from 'moment';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  TimeScale,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  TimeScale,
+  Title,
+  Tooltip,
+  Legend,
+);
 
 interface GraphProps {
   genre: GenreGraphData | null
 }
 const Graph: NextPage<GraphProps> = (props: any) => {
   const { genre } = props;
+  const data = {
+    datasets: [
+      {
+        label: 'graph data',
+        data: genre,
+        borderColor: 'rgb(255, 99, 132)',
+      }
+    ],
+  };
+
+  const options: any = {
+    responsive: true,
+    plugins: {
+      legend: false,
+      title: {
+        display: true,
+        text: 'Popularity of x over time',
+      },
+    },
+  };
+
 
   return (
-    <p>{JSON.stringify(genre)}</p>
+    <Line options={options} data={data} />
   )
 }
 
